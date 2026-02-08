cb02b637ed0784067d0b29466a8b1eb8b5c0460c
srishtimaheshwari14 <maheshwarisrishti613@gmail.com>
2026-02-08 09:21:31 +0530

    Update banarasi.js

--- assets/js/banarasi.js
+++ assets/js/banarasi.js
@@ -1,18 +1,15 @@
 const banarasiData = [
-  { img:"https://images.unsplash.com/photo-1598514982841-8d94d5bcb98b", price:"₹4,499" },
-  { img:"https://images.unsplash.com/photo-1600185365483-26d7a4cc7519", price:"₹5,299" },
-  { img:"https://images.unsplash.com/photo-1583391733956-375b2a3f7c04", price:"₹6,199" },
-  { img:"https://images.unsplash.com/photo-1610030469983-98e550d6193c", price:"₹5,999" }
+  { img: "https://images.unsplash.com/photo-1598514982841-8d94d5bcb98b", price: 4499 },
+  { img: "https://images.unsplash.com/photo-1600185365483-26d7a4cc7519", price: 5299 },
+  { img: "https://images.unsplash.com/photo-1583391733956-375b2a3f7c04", price: 6199 },
+  { img: "https://images.unsplash.com/photo-1610030469983-98e550d6193c", price: 5999 }
 ];
 
 let banarasiIndex = 0;
 
-const banarasiImg = document.getElementById("banarasiImg");
-const banarasiPrice = document.getElementById("banarasiPrice");
-
 function showBanarasi(){
-  banarasiImg.src = banarasiData[banarasiIndex].img;
-  banarasiPrice.innerText = banarasiData[banarasiIndex].price;
+  document.getElementById("banarasiImg").src = banarasiData[banarasiIndex].img;
+  document.getElementById("banarasiPrice").innerText = "₹" + banarasiData[banarasiIndex].price;
 }
 
 function banarasiNext(){
@@ -25,8 +22,8 @@ function banarasiPrev(){
   showBanarasi();
 }
 
-function orderBanarasi(){
-  alert("🛍️ Order received!\nWe’ll contact you on WhatsApp 💖");
+function addBanarasiToCart(){
+  addToCart("Banarasi Saree", banarasiData[banarasiIndex].price);
 }
 
 showBanarasi();
